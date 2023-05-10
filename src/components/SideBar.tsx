293c3ca5fda1a9a6c0014067095787ffaaf9d92e
newt239 <ysti2019.win2020.st@gmail.com>
2023-05-11 00:10:10 +0900

    add: link to pinned list

--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,12 +1,10 @@
 import { Link } from "react-router-dom";
 
-import { Navbar, Textarea } from "@mantine/core";
+import { Box, List, Navbar, Textarea } from "@mantine/core";
 
 import { useAtom, useAtomValue } from "jotai";
 
 import { openedAtom, pinnedFontsAtom, textAtom } from "~/jotai/atoms";
-
-import classes from "./SideBar.module.css";
 
 const SideBar: React.FC = () => {
   const [text, setText] = useAtom(textAtom);
@@ -20,24 +18,26 @@ const SideBar: React.FC = () => {
       width={{ sm: 200, lg: 300 }}
       p="sm"
     >
-      <Textarea
-        label="表示するテキスト"
-        autosize
-        value={text}
-        onChange={(e) => setText(e.target.value)}
-      />
-      {pinnedFonts.length > 0 && (
-        <div className={classes.pinnedFontSection}>
-          <Link to="/pinned">
-            <h2>Pinned Fonts</h2>
-          </Link>
-          <ul className={classes.pinnedFontList}>
-            {pinnedFonts.map((font) => (
-              <li key={font}>{font}</li>
-            ))}
-          </ul>
-        </div>
-      )}
+      <Navbar.Section grow>
+        <Textarea
+          label="表示するテキスト"
+          autosize
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        {pinnedFonts.length > 0 && (
+          <Box mt={10}>
+            <Link to="/pinned">Pinned Fonts</Link>
+            <List>
+              {pinnedFonts.map((font) => (
+                <List.Item key={font}>
+                  <Link to={`/font/${font}`}>{font}</Link>
+                </List.Item>
+              ))}
+            </List>
+          </Box>
+        )}
+      </Navbar.Section>
     </Navbar>
   );
 };
