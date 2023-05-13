0864557dc8c540bd867731a4b7830edb28d5d931
newt239 <ysti2019.win2020.st@gmail.com>
2023-05-13 17:16:30 +0900

    add: info tab to font page

--- src/pages/Font.tsx
+++ src/pages/Font.tsx
@@ -1,7 +1,16 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
-import { Box, ScrollArea, Stack, Tabs, Text, Title } from "@mantine/core";
+import {
+  Box,
+  ScrollArea,
+  Select,
+  Stack,
+  Table,
+  Tabs,
+  Text,
+  Title,
+} from "@mantine/core";
 
 import { useAtomValue } from "jotai";
 import opentype from "opentype.js";
@@ -12,6 +21,10 @@ import { FontData } from "~/types/FontData";
 const FontPage: React.FC = () => {
   const { fontFamily } = useParams();
   const [fonts, setFonts] = useState<FontData[]>([]);
+  const [fontMeta, setFontMeta] = useState<{
+    index: number;
+    data: opentype.Font;
+  } | null>(null);
   const text = useAtomValue(textAtom);
 
   const loadFontData = async () => {
@@ -25,7 +38,8 @@ const FontPage: React.FC = () => {
     setFonts(filledFonts);
     const blob = await filledFonts[0].blob();
     const font = opentype.parse(await blob.arrayBuffer());
-    console.log(font);
+    setFontMeta({ index: 0, data: font });
+    console.log(font.tables.name);
   };
 
   useEffect(() => {
@@ -43,26 +57,57 @@ const FontPage: React.FC = () => {
           <Tabs defaultValue="info" variant="outline">
             <Tabs.List>
               <Tabs.Tab value="info">Info</Tabs.Tab>
-              <Tabs.Tab value="weights">Weights</Tabs.Tab>
+              <Tabs.Tab value="variants">Variants</Tabs.Tab>
               <Tabs.Tab value="lab">Lab</Tabs.Tab>
             </Tabs.List>
 
             <Tabs.Panel value="info" pt="xs">
-              Gallery tab content
+              <Select
+                label="ウエイトを選択"
+                placeholder="Pick one"
+                defaultValue="0"
+                data={fonts.map((font, i) => {
+                  return { value: i.toString(), label: font.style };
+                })}
+                onChange={async (event) => {
+                  const index = Number(event);
+                  const blob = await fonts[index].blob();
+                  const font = opentype.parse(await blob.arrayBuffer());
+                  setFontMeta({ index, data: font });
+                }}
+                py={10}
+              />
+              {fontMeta && (
+                <Table>
+                  <tbody>
+                    {Object.entries(fontMeta.data.names).map(([key, value]) => (
+                      <tr key={key}>
+                        <td>{key}</td>
+                        <td>{value.en}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </Table>
+              )}
             </Tabs.Panel>
 
-            <Tabs.Panel value="weights" pt="xs">
+            <Tabs.Panel value="variants" pt="xs">
               <Stack>
                 {fonts.map((font) => (
                   <Box key={font.fullName}>
                     <Title order={3}>{font.fullName}</Title>
+                    {font.style}
                     <ScrollArea w="100%">
                       <Text
                         fz={50}
                         sx={{ whiteSpace: "nowrap" }}
                         style={{
-                          fontFamily: font.fullName,
+                          fontFamily: font.family,
                           fontWeight: font.style,
+                          fontStyle: font.style.includes("Italic")
+                            ? "italic"
+                            : "normal",
+                          fontSynthesis: "none",
                         }}
                       >
                         {text}
