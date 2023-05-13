b4e247134aa55973651d0a73224f40e3172eb5fe
newt239 <ysti2019.win2020.st@gmail.com>
2023-05-13 18:37:18 +0900

    add: refetch font button

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,11 +1,10 @@
-import { useEffect } from "react";
-
-import { Button, Center, Grid } from "@mantine/core";
+import { Box, Button, Center, Grid, LoadingOverlay } from "@mantine/core";
+import { useDisclosure } from "@mantine/hooks";
 
 import { useAtom, useAtomValue } from "jotai";
+import { ArrowClockwise, Play } from "phosphor-react";
 
-import FontCard from "../components/FontCard";
-
+import FontCard from "~/components/FontCard";
 import { fontListAtom, fontNameListAtom, pinnedFontsAtom } from "~/jotai/atoms";
 import { FontData } from "~/types/FontData";
 
@@ -13,8 +12,10 @@ const FontList: React.FC<{ pinned?: boolean }> = ({ pinned = false }) => {
   const pinnedFonts = useAtomValue(pinnedFontsAtom);
   const [fontList, setFontList] = useAtom(fontListAtom);
   const [fontNameList, setFontNameList] = useAtom(fontNameListAtom);
+  const [visible, handlers] = useDisclosure(false);
 
   const logFontData = async () => {
+    handlers.open();
     setFontList([]);
     setFontNameList([]);
     try {
@@ -32,37 +33,54 @@ const FontList: React.FC<{ pinned?: boolean }> = ({ pinned = false }) => {
         ]);
       }
     } catch (err) {
-      console.error(err);
+      alert(err);
     }
+    handlers.close();
   };
 
-  useEffect(() => {
-    logFontData();
-  }, [pinned]);
-
   return (
-    <>
+    <Box>
+      <LoadingOverlay
+        visible={visible}
+        overlayBlur={2}
+        loaderProps={{ size: "lg", color: "yellow" }}
+      />
       {fontNameList.length === 0 ? (
         <Center>
-          <Button color="yellow" onClick={logFontData}>
-            フォントを取得する
+          <Button
+            color="yellow"
+            onClick={logFontData}
+            leftIcon={<Play size={20} />}
+          >
+            フォントを取得
           </Button>
         </Center>
       ) : (
-        <Grid m={4}>
-          {fontNameList.map((fontName) => {
-            const fontData = fontList.find(
-              (font) => font.family === fontName
-            ) as FontData;
-            return (
-              <Grid.Col lg={3} md={4} sm={6} xs={12} key={fontName}>
-                <FontCard key={fontName} family={fontData.family} />
-              </Grid.Col>
-            );
-          })}
-        </Grid>
+        <>
+          <Box ta="end">
+            <Button
+              color="yellow"
+              onClick={logFontData}
+              leftIcon={<ArrowClockwise size="20" />}
+            >
+              フォントを再取得
+            </Button>
+          </Box>
+          <Grid m={4}>
+            {fontNameList.map((fontName) => {
+              const fontData = fontList.find(
+                (font) => font.family === fontName
+              ) as FontData;
+              return (
+                <Grid.Col lg={3} md={4} sm={6} xs={12} key={fontName}>
+                  <FontCard key={fontName} family={fontData.family} />
+                </Grid.Col>
+              );
+            })}
+          </Grid>
+        </>
       )}
-    </>
+    </Box>
   );
 };
 
