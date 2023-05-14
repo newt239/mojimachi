25c23908d41517dbcc709b85d5ad65492f9b8b12
newt239 <ysti2019.win2020.st@gmail.com>
2023-05-14 17:45:25 +0900

    add: filter by ja

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -6,10 +6,11 @@ import { useDisclosure } from "@mantine/hooks";
 
 import { ArrowClockwise, Play } from "@phosphor-icons/react";
 import { useAtom, useAtomValue } from "jotai";
+import opentype from "opentype.js";
 
 import FontCard from "~/components/FontCard";
-import { fontListAtom, fontNameListAtom, pinnedFontsAtom } from "~/jotai/atoms";
-import { FontData } from "~/types/FontData";
+import { fontListAtom, pinnedFontsAtom } from "~/jotai/atoms";
+import { FontData, FontList } from "~/types/FontData";
 
 const HomePage: React.FC = () => {
   const { search } = useLocation();
@@ -17,7 +18,6 @@ const HomePage: React.FC = () => {
   const query = new URLSearchParams(search);
   const pinnedFonts = useAtomValue(pinnedFontsAtom);
   const [fontList, setFontList] = useAtom(fontListAtom);
-  const [fontNameList, setFontNameList] = useAtom(fontNameListAtom);
   const [visible, handlers] = useDisclosure(false);
 
   const ref = useRef(true);
@@ -27,27 +27,61 @@ const HomePage: React.FC = () => {
       ref.current = false;
       return;
     } else {
-      logFontData();
+      getLocalFonts();
     }
   }, [search]);
 
-  const logFontData = async () => {
+  const getLocalFonts = async () => {
     handlers.open();
     setFontList([]);
-    setFontNameList([]);
     try {
-      const availableFonts: FontData[] = await window.queryLocalFonts();
-      if (query.get("pinned") === "true") {
-        setFontList(
-          availableFonts.filter((font) => pinnedFonts.includes(font.family))
-        );
-        setFontNameList(pinnedFonts);
-      } else {
-        setFontList(availableFonts);
-        setFontNameList([
-          ...new Set(availableFonts.map((font) => font.family)),
-        ]);
-      }
+      const fonts: FontData[] = await window.queryLocalFonts();
+      const uniqueFonts = Array.from(
+        new Map(fonts.map((font) => [font.family, font])).values()
+      );
+      const parsedFonts: FontList = await Promise.all(
+        uniqueFonts.map(async (font) => {
+          const blob = await font.blob();
+          try {
+            const fontData = opentype.parse(await blob.arrayBuffer());
+            if (fontData.supported) {
+              const glyph_あ = fontData.charToGlyphIndex("あ");
+              if (glyph_あ !== 0) {
+                return {
+                  family: font.family,
+                  postscriptName: font.postscriptName,
+                  ja: "supported",
+                };
+              }
+            }
+            return font;
+          } catch (err) {
+            return {
+              family: font.family,
+              postscriptName: font.postscriptName,
+              ja: "undetermind",
+            };
+          }
+        })
+      );
+      const filteredFonts = parsedFonts.filter((font) => {
+        if (
+          query.get("pinned") === "true" &&
+          !pinnedFonts.includes(font.family)
+        ) {
+          return false;
+        }
+        if (query.get("ja") === "true" && !font?.ja) {
+          return false;
+        }
+        return true;
+      });
+      const sortedFonts = filteredFonts.sort((a, b) => {
+        if (a.ja === "supported" && b.ja === "undetermind") return -1;
+        if (a.ja === "undetermind" && b.ja === "supported") return 1;
+        return 1;
+      });
+      setFontList(sortedFonts);
     } catch (err) {
       alert(err);
     }
@@ -61,11 +95,11 @@ const HomePage: React.FC = () => {
         overlayBlur={2}
         loaderProps={{ size: "lg", color: "yellow" }}
       />
-      {fontNameList.length === 0 ? (
+      {fontList.length === 0 ? (
         <Center>
           <Button
             color="yellow"
-            onClick={logFontData}
+            onClick={getLocalFonts}
             leftIcon={<Play size={20} />}
           >
             フォントを取得
@@ -76,23 +110,18 @@ const HomePage: React.FC = () => {
           <Box ta="end" mx={5}>
             <Button
               color="yellow"
-              onClick={logFontData}
+              onClick={getLocalFonts}
               leftIcon={<ArrowClockwise size="20" />}
             >
               フォントを再取得
             </Button>
           </Box>
           <Grid m={5}>
-            {fontNameList.map((fontName) => {
-              const fontData = fontList.find(
-                (font) => font.family === fontName
-              ) as FontData;
-              return (
-                <Grid.Col lg={3} md={4} sm={6} xs={12} key={fontName}>
-                  <FontCard key={fontName} family={fontData.family} />
-                </Grid.Col>
-              );
-            })}
+            {fontList.map((font) => (
+              <Grid.Col lg={3} md={4} sm={6} xs={12} key={font.family}>
+                <FontCard family={font.family} />
+              </Grid.Col>
+            ))}
           </Grid>
         </>
       )}
