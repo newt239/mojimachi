7be8e7da6fd188dd8072f37133929ba1c052ddcb
newt239 <ysti2019.win2020.st@gmail.com>
2023-06-24 23:42:49 +0900

    remove: all files

--- vite.config.ts
+++
@@ -1,9 +0,0 @@
-import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.GITHUB_PAGES ? "local-font-emulator" : "./",
-  plugins: [react(), tsconfigPaths()],
-});
