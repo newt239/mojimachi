69bcc183577b013576b14ff8a988c40463c051d6
newt239 <ysti2019.win2020.st@gmail.com>
2023-06-30 18:33:48 +0900

    add: preset texts

--- src/components/Menubar.tsx
+++ src/components/Menubar.tsx
@@ -3,12 +3,20 @@ import { useState } from "react";
 import {
   Box,
   Flex,
+  IconButton,
   Input,
+  InputGroup,
+  InputRightElement,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuList,
   Slider,
   SliderFilledTrack,
   SliderThumb,
   SliderTrack,
 } from "@chakra-ui/react";
+import { CaretDown } from "@phosphor-icons/react";
 import { useAtom, useSetAtom } from "jotai";
 
 import { fontSizeAtom, previewStringAtom } from "~/utils/jotai";
@@ -23,6 +31,13 @@ const Menubar: React.FC = () => {
     setFontSize(e);
   };
 
+  const sampleTexts = [
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz",
+    "0123456789 ¿ ? ¡ ! & @ ‘ ’ “ ” « » % * ^ # $ £ € ¢ / ( ) [ ] { } . , ® ©",
+    "The quick brown fox jumps over the lazy dog.",
+    "あのイーハトーヴォのすきとおった風、夏でも底に冷たさをもつ青いそら、うつくしい森で飾られたモリーオ市、郊外のぎらぎらひかる草の波。",
+  ];
+
   return (
     <Flex
       bg="gray.900"
@@ -34,28 +49,50 @@ const Menubar: React.FC = () => {
       pt="0"
       gap="1rem"
     >
-      <Box w="4rem" m="auto">
-        {localFontSize}px
-      </Box>
-      <Slider
-        aria-label="slider-ex-1"
-        defaultValue={localFontSize}
-        onChange={onChange}
-        min={10}
-        max={100}
-        focusThumbOnChange={false}
-      >
-        <SliderTrack>
-          <SliderFilledTrack />
-        </SliderTrack>
-        <SliderThumb />
-      </Slider>
-      <Input
-        placeholder="プレビューする文字"
-        value={previewString}
-        onChange={(e) => setPreviewString(e.target.value)}
-        variant="outline"
-      />
+      <Flex w="50%">
+        <Box w="4rem" m="auto">
+          {localFontSize}px
+        </Box>
+        <Slider
+          aria-label="slider-ex-1"
+          defaultValue={localFontSize}
+          onChange={onChange}
+          min={10}
+          max={100}
+          focusThumbOnChange={false}
+        >
+          <SliderTrack>
+            <SliderFilledTrack />
+          </SliderTrack>
+          <SliderThumb />
+        </Slider>
+      </Flex>
+      <Flex w="50%">
+        <InputGroup>
+          <Input
+            placeholder="プレビューする文字"
+            value={previewString}
+            onChange={(e) => setPreviewString(e.target.value)}
+            variant="outline"
+          />
+          <InputRightElement>
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                icon={<CaretDown />}
+                variant="ghost"
+              />
+              <MenuList>
+                {sampleTexts.map((text) => (
+                  <MenuItem key={text} onClick={() => setPreviewString(text)}>
+                    {text}
+                  </MenuItem>
+                ))}
+              </MenuList>
+            </Menu>
+          </InputRightElement>
+        </InputGroup>
+      </Flex>
     </Flex>
   );
 };
