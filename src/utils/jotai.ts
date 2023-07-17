6d46cd2b851a3f9ffa5a7a11d868cdfecc9446b8
newt239 <ysti2019.win2020.st@gmail.com>
2023-07-17 22:33:11 +0900

    change: default preview text

--- src/utils/jotai.ts
+++ src/utils/jotai.ts
@@ -4,6 +4,8 @@ export const familyKeywordAtom = atom<string>("");
 
 export const jaFilterAtom = atom<boolean>(false);
 
-export const previewStringAtom = atom<string>("日本語どう？");
+export const previewStringAtom = atom<string>(
+  "The quick brown fox jumps over the lazy dog."
+);
 
 export const favoriteFamiliesAtom = atom<string[]>([]);
